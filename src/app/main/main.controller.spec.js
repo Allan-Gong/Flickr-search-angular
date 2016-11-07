0ce7a04a8e48bdf1b601a0bac557beda4e4f3964
Allan Gong <Hanyun.Gong@monash.edu>
2016-11-07 22:20:18 +1100

    Implement unit test cases for main controller

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,32 +1,113 @@
-describe('controllers', () => {
+describe('main.controller', () => {
   let vm;
+  var $httpBackend, $rootScope;
 
-  beforeEach(angular.mock.module('angularGulp'));
+  beforeEach(angular.mock.module('angularFlickrSearch'));
 
-  beforeEach(inject(($controller, webDevTec, toastr) => {
-    spyOn(webDevTec, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-    spyOn(toastr, 'info').and.callThrough();
+  beforeEach(inject(($injector) => {
+    $httpBackend = $injector.get('$httpBackend');
 
-    vm = $controller('MainController');
+    $rootScope = $injector.get('$rootScope');
+
+    var $controller = $injector.get('$controller');
+
+    vm = $controller('MainController', {'$scope' : $rootScope });
   }));
 
-  it('should have a timestamp creation date', () => {
-    expect(vm.creationDate).toEqual(jasmine.any(Number));
+  describe('on success', () => {
+
+
+
+    it('should return items on search() when searchTerm is equal to or greater than 3 letters', () => {
+
+      var returnedItems = [
+        {
+          'title': 'Test title 1',
+          'author': 'Test author 1',
+          'link': 'https://www.flickr.com/services/api/dummy-link-1/',
+          'media': { 'm': 'https://www.flickr.com/services/api/image/test1.png' },
+          'tags': 'tag11 tag12'
+        },
+        {
+          'title': 'Test title 2',
+          'author': 'Test author 2',
+          'link': 'https://www.flickr.com/services/api/dummy-link-2/',
+          'media': { 'm': 'https://www.flickr.com/services/api/image/test2.png' },
+          'tags': 'tag21 tag22'
+        }
+      ];
+
+      $httpBackend.whenJSONP(/.+/).respond(200, {
+        'items': returnedItems
+      });
+
+      vm.searchTerm = 'nba';
+      vm.search();
+      $httpBackend.flush();
+
+      var expectedItems = [
+        {
+          'title': 'Test title 1',
+          'author': 'Test author 1',
+          'link': 'https://www.flickr.com/services/api/dummy-link-1/',
+          'media': { 'm': 'https://www.flickr.com/services/api/image/test1.png' },
+          'tags': ['tag11', 'tag12']
+        },
+        {
+          'title': 'Test title 2',
+          'author': 'Test author 2',
+          'link': 'https://www.flickr.com/services/api/dummy-link-2/',
+          'media': { 'm': 'https://www.flickr.com/services/api/image/test2.png' },
+          'tags': ['tag21', 'tag22']
+        }
+      ];
+
+      expect(vm.items).toEqual(expectedItems);
+    });
+
+    it('should return null on search() when searchTerm is less than 3 letters', () => {
+      vm.searchTerm = 'na';
+      vm.search();
+      expect(vm.items).toEqual([]);
+    });
+
   });
 
-  it('should define animate class after delaying timeout', inject($timeout => {
-    $timeout.flush();
-    expect(vm.classAnimation).toEqual('rubberBand');
-  }));
+  describe('on failure', () => {
+    it('should return an error item when API call failed', () => {
+      $httpBackend.whenJSONP(/.+/).respond(500, {
+        'error': 'Opps'
+      });
 
-  it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
-    vm.showToastr();
-    expect(toastr.info).toHaveBeenCalled();
-    expect(vm.classAnimation).toEqual('');
-  }));
+      vm.searchTerm = 'nba';
+      vm.search();
+      $httpBackend.flush();
 
-  it('should define more than 5 awesome things', () => {
-    expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-    expect(vm.awesomeThings.length === 5).toBeTruthy();
+      expect(vm.items.length).toEqual(1);
+
+      var errorItem = vm.items[0];
+
+      expect(errorItem.tags).toEqual(['error', '502']);
+
+    });
+
+    it('should return an error item when API call returns no results', () => {
+      $httpBackend.whenJSONP(/.+/).respond(200, {
+        'items': []
+      });
+
+      vm.searchTerm = 'nba';
+      vm.search();
+      $httpBackend.flush();
+
+      expect(vm.items.length).toEqual(1);
+
+      var errorItem = vm.items[0];
+
+      expect(errorItem.tags).toEqual(['no-results']);
+
+    });
   });
+
+
 });
